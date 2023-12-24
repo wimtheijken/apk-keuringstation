065031aad6fd90ba59668d3787bd54b9e5a0bc09
wimtheijken <info@theijken.nl>
2023-12-24 12:35:57 +0100

    start

---
+++ src/test/java/nl/theijken/apkkeuringsation/ApkKeuringsationApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.theijken.apkkeuringsation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApkKeuringsationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
