4d3d1762d33b33ddac4b4780bb7cce3ce749a4e0
wimtheijken <info@theijken.nl>
2023-12-26 11:51:31 +0100

    data.sql met gebruikers gevuld

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,10 +2,18 @@
 
 -- noinspection SqlNoDataSourceInspectionForFile
 
+insert into roles(rolename) values  ('ROLE_ADMIN'), ('ROLE_MECHANIC'), ('ROLE_CASHIER');
 
-insert into roles(rolename) values ('ROLE_USER'), ('ROLE_ADMIN');
+--   username: admin, password: admin
+insert into users(username, password) values ('admin', '$2a$12$qrE6TwOWnTQJiJa7TNC7/.k127kBw1NEPoGOeykZ8hzmqN3baG4xS');
 
---   username: karel, password: appel
-insert into users(username, password) values ('karel', '$2a$12$v3hpM1z6mh.ITK9UdFeeiOHOaRzvrlLCCGQc9tyZi718XWXWmLub6');
+--   username: Piet, password: password
+insert into users(username, password) values ('Piet', '$2a$12$Qb.mt09Z7zU20EPy3PtilOCEi.yBAkzBOUzk1yd4b5UL/2tAVFJgm');
 
-insert into users_roles(username, rolename) values('karel', 'ROLE_USER');
+--   username: Mirjam, password: password
+insert into users(username, password) values ('Mirjam', '$2a$12$Qb.mt09Z7zU20EPy3PtilOCEi.yBAkzBOUzk1yd4b5UL/2tAVFJgm');
+
+
+insert into users_roles(username, rolename) values('admin', 'ROLE_ADMIN');
+insert into users_roles(username, rolename) values('Piet', 'ROLE_MECHANIC');
+insert into users_roles(username, rolename) values('Mirjam', 'ROLE_CASHIER');
